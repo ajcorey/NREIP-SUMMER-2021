36ee8a8180b729ef26046cce0fdd9f4621fbcb25
Andrew <andrewmsullivan27@gmail.com>
2021-06-29 11:14:54 -0400

    Update readData.py

--- BrainControl/ReadData/readData.py
+++ BrainControl/ReadData/readData.py
@@ -7,15 +7,19 @@ import csv
 import pandas as pd
 
 
+# Set variable for the filename to make changing easier later on
+fileName = 'SoniaThinking.csv'
+outputFileName = " ".join((fileName, 'output'))
 
 # Make the DataFrame, read the .CSV, and skip the header and information rows
-df = pd.read_csv('SoniaThinking.csv', skiprows=5)
+df = pd.read_csv(fileName, skiprows=5)
 
 
 # Delete unnecessary columns
-df = .drop(df.columns[[0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]], axis=1)
+df = df.drop(df.columns[[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]], axis=1)
 
 
 # Convert DataFrame to a .CSV
-df.to_csv('output.csv', index=False)
+df.to_csv(outputFileName, index=False)
+
 
