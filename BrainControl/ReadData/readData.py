470d8a566329140bc430ba2b1657aac3f6d87985
Andrew <andrewmsullivan27@gmail.com>
2021-06-29 13:34:44 -0400

    Update readData.py

--- BrainControl/ReadData/readData.py
+++ BrainControl/ReadData/readData.py
@@ -5,9 +5,10 @@
 # Import the libraries that we need
 import csv
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 from datetime import datetime
+
 
 
 
@@ -18,35 +19,29 @@ fileName = 'SoniaThinking.csv'
 outputFileName = "".join(('SoniaThinking', 'Output.csv'))
 
 # Make the DataFrame, read the .CSV, and skip the header and information rows
-df = pd.read_csv(fileName, skiprows=5)
+df = pd.read_csv(fileName, skiprows = 5, header = None)
 
 
-# Delete unnecessary columns
+# Delete unnecessary columns and rename the remaining ones
 df = df.drop(df.columns[[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]], axis=1)
+df.columns = ['Channel1', 'Timestamp']
+
+# Convert the formatted timestamp to UNIX
+for i in range(0, len(df)):
+    formattedDate = df.iloc[i, 1]
+    unixDate = datetime.strptime(formattedDate, ' %Y-%m-%d %H:%M:%S.%f').strftime("%s.%f")[:-3}
+    df.iat[i, 1] = unixDate
 
 
 # Convert DataFrame to a .CSV
-df.to_csv(outputFileName, index=False)
-
-
-# Convert the formatted timestamp to UNIX
-
-for i in range(len(df)):
-    formattedDate = df.iloc[i, 1]
-    unixDate = datetime.strptime(formattedDate, '%Y-%m-%d %H:%M:%S.%f').strftime("%s.%f")
-    print(unixDate)
-
+df.to_csv(outputFileName, index = False)
 
 # Graph the data
-
-# x, y = np.loadtxt(outputFileName, delimiter=',', unpack=True)
-# plt.plot(x,y, label='Loaded from file!')
-
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Interesting Graph\nCheck it out')
-# plt.legend()
-# plt.show()
-
-
-
+x, y = np.loadtxt(outputFileName, delimiter=',', unpack=True)
+print('File loaded!')
+plt.plot(x,y, label='Loaded from file!')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
