d0cffd05b20538ce8914f0f0f0faa58719f2066a
Andrew <andrewmsullivan27@gmail.com>
2021-06-29 10:54:47 -0400

    Update readData.py

--- BrainControl/ReadData/readData.py
+++ BrainControl/ReadData/readData.py
@@ -13,8 +13,9 @@ df = pd.read_csv('SoniaThinking.csv', skiprows=5)
 
 
 # Delete unnecessary columns
-df.drop(df.columns[[0,21], axis=1) # Delete the "Sample Index" columns
+df = .drop(df.columns[[0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]], axis=1)
 
 
 # Convert DataFrame to a .CSV
 df.to_csv('output.csv', index=False)
+
