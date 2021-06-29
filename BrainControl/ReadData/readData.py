b910d68a2d6bb44d46b10e3dde32398f17e52ab7
Andrew <andrewmsullivan27@gmail.com>
2021-06-29 11:21:28 -0400

    Update readData.py

--- BrainControl/ReadData/readData.py
+++ BrainControl/ReadData/readData.py
@@ -5,11 +5,13 @@
 # Import the libraries that we need
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Set variable for the filename to make changing easier later on
 fileName = 'SoniaThinking.csv'
-outputFileName = " ".join((fileName, 'output'))
+outputFileName = " ".join(('SoniaThinking, 'output.csv'))
 
 # Make the DataFrame, read the .CSV, and skip the header and information rows
 df = pd.read_csv(fileName, skiprows=5)
@@ -21,5 +23,17 @@ df = df.drop(df.columns[[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,2
 
 # Convert DataFrame to a .CSV
 df.to_csv(outputFileName, index=False)
+
+# Graph the data
+
+x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+plt.plot(x,y, label='Loaded from file!')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
+
 
 
