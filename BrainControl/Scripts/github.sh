7250a8e9351061ffe25d67556775ed37bbac9904
Andrew <andrewmsullivan27@gmail.com>
2021-06-30 13:42:35 -0400

    Add files via upload

---
+++ BrainControl/Scripts/github.sh
@@ -0,0 +1,6 @@
+cd ~/git/NREIP-SUMMER-2021
+git pull
+cp ~/git/NREIP-SUMMER-2021/BrainControl/Scripts/readData.py ~/Desktop/ThingsToPlot
+cp -r ~/git/NREIP-SUMMER-2021/BrainControl/Scripts/TestingOutputs ~/Desktop/ThingsToPlot
+exit
+
